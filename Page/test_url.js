d948ac996f4b0654c6b4c6e092a73ae3f9fa540f
iSanax <gracjan.jagiello.dev@gmail.com>
2025-08-04 16:12:26 +0200

    Delete Page/test_url.js

--- Page/test_url.js
+++
@@ -1,8 +0,0 @@
-// const url = 'https://api.github.com/repos/iSanax/GitHub-Pages/contents/Page/Data';
-
-function createURL() {
-    return window.location.pathname
-}
-
-console.log('Path:', createURL();
-console.log('Test:', 1+1);
