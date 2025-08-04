a7eb1688e855465769467a8d4ded163279f627f0
iSanax <gracjan.jagiello.dev@gmail.com>
2025-08-04 16:14:18 +0200

    Update scripts_csv.js

--- Page/scripts_csv.js
+++ Page/scripts_csv.js
@@ -1,6 +1,7 @@
 // const url = 'https://api.github.com/repos/iSanax/GitHub-Pages/contents/Page/Data';
 function createURL() {
-    return window.location.pathname
+    return "111"
+    //return window.location.pathname
 }
 console.log('Path:', createURL();
 
